a4f3f4cbb2d0a8b5befec46fb579effcb3104ff8
Npontiroli21 <npontiroli@estudiantes.unsam.edu.ar>
2024-10-16 10:02:44 -0300

    Avance en las operaciones con 3 tablas

--- codigo_demo_BD_SQL/operaciones_3_tablas/insercion_3tablas.sql
+++ codigo_demo_BD_SQL/operaciones_3_tablas/insercion_3tablas.sql
@@ -1,1 +1,14 @@
--- Aquí va el código para la inserción de datos sobre tres tablas
+--Insertar una nueva misión y luego eliminarla, involucrando 3 tablas: "mision", "satelite", y "mision_satelite"
+
+INSERT INTO mision (id_mision, nombre_mision, fecha_inicio, fecha_fin, objetivo)
+VALUES (DEFAULT, 'Misión Omega', '2024-01-01', '2024-06-01', 'Exploración del espacio profundo')
+RETURNING id_mision INTO v_id_mision; -- Utilizo una variable temporal para saber el ultimo SERIAL
+
+INSERT INTO mision_satelite (id_mision, id_satelite)
+VALUES (v_id_mision, 1);
+
+DELETE FROM mision_satelite
+WHERE id_mision = v_id_mision;
+
+DELETE FROM mision
+WHERE id_mision = v_id_mision;
